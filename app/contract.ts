87bf7c496f0cb58cab68e6594bda1e6bc211e589
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2022-09-12 18:06:37 +0300

    dev

--- app/contract.ts
+++ app/contract.ts
@@ -16,31 +16,25 @@ export async function init(network: NetworkInfo, callback: Callback) {
     );
     lotteryContract.events.Add()
         .on('data', async function (event: any) {
-            const {addAmount} = event.returnValues;
+            const { addAmount } = event.returnValues;
             callback('add', addAmount);
         });
 
     lotteryContract.events.Try()
         .on('data', async function (event: any) {
-            const {tryAmount, count, totalAmount} = event.returnValues;
-            callback('try', tryAmount, count, totalAmount, event.transactionHash);
+            const { tryAmount, count, totalAmount, isWin } = event.returnValues;
+            callback('try', tryAmount, count, totalAmount, event.transactionHash, isWin);
         });
 
     lotteryContract.events.Win()
         .on('data', async function (event: any) {
-            const {winAmount, count} = event.returnValues;
+            const { winAmount, count } = event.returnValues;
             callback('win', winAmount, count, event.transactionHash);
         })
 
-    lotteryContract.events.ChangedRates()
+    lotteryContract.events.SettingsChanged()
         .on('data', async function (event: any) {
-            const {winRate, feeRate} = event.returnValues;
-            console.log('[change rate]', winRate, feeRate);
-        })
-
-    lotteryContract.events.ChangedChance()
-        .on('data', async function (event: any) {
-            const {minChance, maxChance} = event.returnValues;
-            console.log('[change chance]', minChance, maxChance);
+            const { settings } = event.returnValues;
+            console.log('[change settings]', settings);
         })
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,6 +2,7 @@ import { ethers } from "hardhat";
 import fs from 'fs';
 import path from 'path';
 import networks from '../app/networks';
+import {defaultMinRate} from "../test/utils/utils";
 
 const dataDir = 'data';
 if (!fs.existsSync(dataDir)) {
@@ -14,16 +15,21 @@ async function main() {
   const { filename } = networks(HARDHAT_NETWORK);
 
   const [owner] = await ethers.getSigners();
+
+  const Randomizer = await ethers.getContractFactory("RandomizerTest", owner);
+  const randomizer = await Randomizer.deploy();
+  await randomizer.deployed();
+
   const Lottery = await ethers.getContractFactory("Lottery", owner);
   const lottery = await Lottery.deploy({
-    randomValue: 1000,
-    minChance: 1,
-    maxChance: 100,
+    randomValue: 10000,
+    minChance: 10,
+    maxChance: 1000,
     winRate: 90,
-    feeRate: 70,
-    minRate: 10,
+    feeRate: 90,
+    minRate: Number(defaultMinRate),
+    randomizer: randomizer.address,
   });
-
   await lottery.deployed();
 
   console.log(`deploy to:
